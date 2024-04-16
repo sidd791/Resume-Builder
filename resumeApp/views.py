583c33a3afe8d8ed39936e8079cfb4d2b89743a2
Siddharath Singh <siddharathsingh19@gmail.com>
2024-04-16 18:10:12 +0530

    Fixed a bug

--- resumeApp/views.py
+++ resumeApp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from resumeApp.models import Profile
+
 # Create your views here.
+
 def index(request):
     if request.method == "POST":
         name = request.POST.get("name", "")
@@ -13,10 +15,13 @@ def index(request):
         hobbies = request.POST.get("hobbies", "")
         
         profile = Profile(name = name, email = email, ph_num = ph_num, education = education, degree = degree, skills = skills, experience = experience, hobbies = hobbies)
-        profile.save()    
+        profile.save()  
+        return redirect('resumeApp:resume', id=profile.id)
     return render(request, 'cv/accept.html')
-
 
 def resume(request, id):
     profile = Profile.objects.get(pk = id)
     return render(request, 'cv/resume.html', {'profile':profile})
+
+
+
